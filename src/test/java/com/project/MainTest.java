4fd0dd14616ed1b99d41a9a569b4b3c6bc7dbc7c
Emerson Millard <emillard.code@gmail.com>
2024-06-29 12:00:00 -0700

    Introduced JUnit Tests into the project.

--- src/test/java/com/project/MainTest.java
+++ src/test/java/com/project/MainTest.java
@@ -1,11 +1,20 @@
 package com.project;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 
 public class MainTest {
 
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+
+    @Test
     public void testApp() {
-        Assertions.assertTrue( true );
+
+        System.out.println("Hello Baeldung Readers!!");
+        Assert.assertEquals("Hello Baeldung Readers!!", systemOutRule.getLog().trim());
     }
 
 }
